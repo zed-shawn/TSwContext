a25d6d725815e87379ac34a957a6e7de1f80f076
zed.shawn <alizeeshan1999@gmail.com>
2021-07-07 22:09:44 +0530

    added components

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import React, {FC} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import TextInputComp from './components/textInput';
 
 const App: FC = () => {
   return (
-    <View>
-      <Text>dfsdfef ffe</Text>
-      <TextInputComp placeholder="Enter Text Here"/>
+    <View style={styles.root}>
+      <Text>TO-DOist</Text>
+      <View>
+        <TextInputComp placeholder="Enter Text Here" />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
 
 export default App;
