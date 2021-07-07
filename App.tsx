82a4190cd3b2986c32fd122bfc3bfbe6a46e1ac9
zed.shawn <alizeeshan1999@gmail.com>
2021-07-08 02:16:16 +0530

    UI overhaul, flatlist added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
-import React, {FC} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {FC, useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import TextInputComp from './components/textInput';
 import ButtonComp from './components/button';
+import ListTile from './components/listTile';
+
+interface DataStorage {
+  goal: string;
+}
 
 const App: FC = () => {
+  const [addValue, setAddValue] = useState<string>('');
+  const [data, addData] = useState<DataStorage[]>([]);
   const addHandler = (): void => {
-    console.log('clicked');
+    const copy: DataStorage[] = data;
+    const arr: DataStorage = {goal: addValue};
+    setAddValue('');
+    copy.push(arr);
+    addData(copy);
   };
+
+
   return (
     <View style={styles.root}>
-      <Text>TO-DOist</Text>
+      <Text style={styles.header}>TO-DOist</Text>
       <View
         style={{
           flexDirection: 'row',
@@ -17,8 +30,19 @@ const App: FC = () => {
           justifyContent: 'space-between',
           width: '90%',
         }}>
-        <TextInputComp placeholder="Enter Text Here" />
+        <TextInputComp
+          placeholder="Enter Task Here"
+          setter={setAddValue}
+          value={addValue}
+        />
         <ButtonComp title="Add To-Do" onPress={addHandler} />
+      </View>
+      <View style={{marginTop: '10%'}}>
+        <FlatList
+          data={data}
+          renderItem={({item}) => <ListTile goal={item.goal} />}
+          keyExtractor={item => item.goal}
+        />
       </View>
     </View>
   );
@@ -28,6 +52,12 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     alignItems: 'center',
+  },
+  header: {
+    fontSize: 30,
+    marginBottom: '5%',
+    fontWeight: 'bold',
+    color: '#900',
   },
 });
 
