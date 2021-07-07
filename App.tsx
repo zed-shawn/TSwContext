678bc58e647b83f83da898d7e58e99de140653e7
zed.shawn <alizeeshan1999@gmail.com>
2021-07-07 21:26:21 +0530

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {View, Text} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>dfsdfef ffe</Text>
+    </View>
+  );
+};
+
+export default App;
